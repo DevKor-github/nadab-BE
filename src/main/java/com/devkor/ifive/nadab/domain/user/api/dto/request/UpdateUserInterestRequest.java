339d96fea26b90125960f3d9e75dd528a83e2a82
1Seob <moon27456@naver.com>
2025-12-03 02:53:16 +0900

    fix: UpdateUserInterestRequest의 interestCode 필드에 validation 적용

--- src/main/java/com/devkor/ifive/nadab/domain/user/api/dto/request/UpdateUserInterestRequest.java
+++ src/main/java/com/devkor/ifive/nadab/domain/user/api/dto/request/UpdateUserInterestRequest.java
@@ -1,6 +1,7 @@
 package com.devkor.ifive.nadab.domain.user.api.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "유저 관심 주제 업데이트 요청")
 public record UpdateUserInterestRequest(
@@ -8,6 +9,7 @@ public record UpdateUserInterestRequest(
                 description = "유저가 설정하고자 하는 새로운 관심 주제 코드입니다.",
                 example = "RELATIONSHIP"
         )
+        @NotBlank(message = "관심 주제 코드는 필수 입력 값입니다.")
         String interestCode
 ) {
 }
