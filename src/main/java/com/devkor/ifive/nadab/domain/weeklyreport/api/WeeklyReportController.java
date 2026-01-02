68b945828b5abf665f0b7d928cc1ebef5cc94bc7
1Seob <moon27456@naver.com>
2026-01-02 22:30:08 +0900

    refactor(report): WeeklyReportController 스웨거 문서화 보강

--- src/main/java/com/devkor/ifive/nadab/domain/weeklyreport/api/WeeklyReportController.java
+++ src/main/java/com/devkor/ifive/nadab/domain/weeklyreport/api/WeeklyReportController.java
@@ -34,7 +34,7 @@ public class WeeklyReportController {
     @Operation(
             summary = "주간 리포트 생성 시작",
             description = """
-                    사용자의 (지난 주에 대한) 주간 리포트 생성을 시작합니다.
+                    사용자의 (지난 주에 대한) 주간 리포트 생성을 시작합니다. </br>
                     비동기로 처리되기 때문에, id로 주간 리포트 조회 API를 폴링하여 상태를 확인할 수 있습니다.
                     """,
             security = @SecurityRequirement(name = "bearerAuth"),
@@ -79,11 +79,11 @@ public class WeeklyReportController {
     @Operation(
             summary = "id로 주간 리포트 조회",
             description = """
-                    주간 리포트를 id로 조회합니다.
-                    생성 대기 중인 경우 status = "PENDING" 으로 반환됩니다.
-                    생성 진행 중인 경우 status = "IN_PROGRESS" 로 반환됩니다.
-                    생성에 실패한 경우 status = "FAILED" 로 반환됩니다.
-                    생성에 성공한 경우 status = "COMPLETED" 로 반환됩니다.
+                    주간 리포트를 id로 조회합니다. </br>
+                    생성 대기 중인 경우 ```status = "PENDING"``` 으로 반환됩니다. </br>
+                    생성 진행 중인 경우 ```status = "IN_PROGRESS"``` 로 반환됩니다. </br>
+                    생성에 실패한 경우 ```status = "FAILED"``` 로 반환됩니다. </br>
+                    생성에 성공한 경우 ```status = "COMPLETED"``` 로 반환됩니다.
                     """,
             responses = {
                     @ApiResponse(
